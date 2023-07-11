1a301b3b3e7d442204b0c93d705659ffcfa78a4b
IsmaelSDG <102034301+IsmaelSDG@users.noreply.github.com>
2023-07-11 11:05:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
+streamlit.header('Breakfast Menu')
 
-streamlit.text('Onion')
-streamlit.text('Potato')
-streamlit.text('Strawberry')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
