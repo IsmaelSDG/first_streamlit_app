69a6548526a369e809e5e26344243a47250b9137
IsmaelSDG <102034301+IsmaelSDG@users.noreply.github.com>
2023-07-11 11:04:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+
+streamlit.text('Onion')
+streamlit.text('Potato')
+streamlit.text('Strawberry')
